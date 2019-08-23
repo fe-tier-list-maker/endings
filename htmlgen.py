ac99084f6a25155629d0102776b6ff6244e81847
AC <alvernchen@gmail.com>
2019-08-23 01:03:24 -0400

    add endings, filter, wip textfile

---
+++ htmlgen.py
@@ -0,0 +1,6 @@
+names = ["Byleth", "Edelgard", "Dimitri", "Claude", "Rhea", "Hubert", "Dorothea", "Ferdinand", "Bernadetta", "Caspar", "Petra", "Linhardt", "Dedue", "Felix", "Mercedes", "Ashe", "Annette", "Sylvain", "Ingrid", "Hilda", "Lorenz", "Raphael", "Lysithea", "Ignatz", "Marianne", "Leonie", "Manuela", "Hanneman", "Catherine", "Shamir", "Alois", "Gilbert", "Cyril", "Flayn", "Seteth", "Sothis"]
+
+for name in names:
+    # print "<button class=\"filterButton\" onclick=\"filterSelection(\'" + name + "\')\">" + name + "</button>"
+    # print "<div class=\"filterDiv " + name + "\">\n\t<p>\n\t</p>\n</div>"
+    print "<div class=\"filterDiv Byleth " + name + "\">\n\t<p>\n\t</p>\n</div>"
